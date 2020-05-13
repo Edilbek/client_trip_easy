b93d60dabfe64a2eff101db20799b133626f3e7f
Edilbek <edil.talantbekov@gmail.com>
2020-05-13 22:04:55 +0600

    fix error with logo 404

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <router-link :to="{name: 'home'}" class="navbar-brand"><img src="src/assets/logo1.png" alt=""></router-link>
+        <router-link :to="{name: 'home'}" class="navbar-brand"><img src="../assets/logo1.png" alt=""></router-link>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
